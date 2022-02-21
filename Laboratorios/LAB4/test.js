6b1624b2b4f743d25bf34f0bfd21a5480bdc77aa
Felix <A01201946@itesm.mx>
2022-02-20 21:58:50 -0600

    jss update

---
+++ Laboratorios/LAB4/test.js
@@ -0,0 +1,9 @@
+document.getElementById("h1").style.fontSize = "25px";
+
+function dispMsg() { 
+   window.alert("This message isnt actually sent") 
+  }
+
+  function clearContent() {
+    document.getElementsByTagName('textarea').innerHTML = ""
+  }
