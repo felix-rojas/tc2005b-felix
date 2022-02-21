4f6780500bba1806d704bca6943e12388b999517
Felix <A01201946@itesm.mx>
2022-02-20 23:14:19 -0600

    javascript prime table

--- Laboratorios/LAB4/test.js
+++ Laboratorios/LAB4/test.js
@@ -1,10 +1,25 @@
 document.getElementById("h1").style.fontSize = "25px";
 
-function dispMsg() { 
-   window.alert("This message isnt actually sent") 
-  }
+function dispMsg() {
+    window.alert("This message isnt actually sent")
+}
 
 function clearContent() {
     var textarea = document.getElementById('textBox');
     textarea.value = "";
-} 
+}
+
+function tableGen()
+{
+rows = window.prompt("Input number of primes", 1);
+const columns = 2;
+  
+ for(var r=0;r<parseInt(rows,10);r++)
+  {
+   var x=document.getElementById('primeTable').insertRow(r);
+     var y = x.insertCell(0);
+     var z = x.insertCell(1);
+     y.innerHTML=r+1;
+     z.innerHTML=(r+1)*(r+1);
+   }
+}
