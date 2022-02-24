c58f64211e747a00f7d4f814fede40506c9f1b73
Felix <a01201946@itesm.mx>
2022-02-24 12:29:55 -0600

    async and sync notes

--- Laboratorios/LAB8/backend/app.js
+++ Laboratorios/LAB8/backend/app.js
@@ -3,4 +3,9 @@ console.log("Hello World");
 // fs: fylesystem
 // we use require to extract a specific package from a module
 // assign to constant so things dont get fucky wucky
-const fylesystem = require('fs');
+const filesystem = require('fs');
+
+// writes a file, content
+// we use the synchronous function and the instructions run until they are done
+filesystem.writeFileSync('hello.txt','Hello world from NODE.js')
+
