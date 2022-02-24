c722469a2ced653940be58a0fab0573369d0b1e4
Felix <a01201946@itesm.mx>
2022-02-24 12:40:31 -0600

    async sorting of items

--- Laboratorios/LAB8/backend/app.js
+++ Laboratorios/LAB8/backend/app.js
@@ -9,3 +9,14 @@ const filesystem = require('fs');
 // we use the synchronous function and the instructions run until they are done
 filesystem.writeFileSync('hello.txt','Hello world from NODE.js')
 
+// node js is constantly executing so it checks everything in the priority queue
+const array = [5000,50,70,100,20,20,10]
+
+for (let item of array){
+    setTimeout(()=>{
+        console.log(item);
+    }, item);
+}
+
+// databse reading must be async so it goes faster, it can retrieve 
+// the most important data
