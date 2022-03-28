6248b2282fe505f22020178d0292b39719a77798
felix-rojas <A01201946@tec.mx>
2022-03-28 09:08:31 -0600

    trying to finish lab 11

--- Laboratorios/LAB11/app.js
+++ Laboratorios/LAB11/app.js
@@ -3,14 +3,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
-var stream = fs.createReadStream("front-end/index.html")
 
-
-// comment after testing
-stream.on("data", function(data) {
-    let chunk = data.toString();
-    console.log(chunk);
-});
+const rutas = require ('./routes/dogs.routes.js')
 
 //Middleware to create server
 app.use((request, response, next) => {
@@ -20,25 +14,7 @@ app.use((request, response, next) => {
 });
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use('/alguna-ruta', (request, response, next) => {
-    console.log(request.body);
-});
-      
-
-// instantiates a route using middleware
-app.use('/corgi', (request, response, next) => {
-    response.send('Respuesta de la ruta "/corgi"');
-    stream;
-    for (let i in dogs) {
-        stream += '<li>' + dogs[i] + '</li>'
-    }
-    stream+='<br><br><a href="nuevo">Agregar un nuevo perrito</a></body>'
-    response.send(stream)
-});
-
-app.use((request, response, next) => {
-    console.log('Another refresh!');
-    response.send('¡Hola mundo!'); //Manda la respuesta
-});
+app.use('/routes', rutas);
+    
 
 app.listen(3000);
